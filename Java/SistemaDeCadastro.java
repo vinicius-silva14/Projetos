9ccc979454e4f73390fa380beff2ae4ddd0cbb38
Vinicius Silva <viniciusdjsilva14@gmail.com>
2025-06-26 18:34:22 -0300

    Update and rename SistemaDeCadastro.java to Java/SistemaDeCadastro.java

--- Java/SistemaDeCadastro.java
+++ Java/SistemaDeCadastro.java
@@ -1,6 +1,6 @@
 public class SistemaDeCadastro {
          public static void main(String[] args) {
-        Cliente cliente = new Cliente("Maria Silva", "12345678901", "Rua das Flores, 123", 3500.00);
+        Cliente cliente = new Cliente("Maria", "12345678901", "Rua das Flores, 123", 3500.00);
         Data dataInicio = new Data(1, 7, 2025); // supondo que a classe Data já está corretamente implementada   
         Emprestimo emprestimo = new Emprestimo(cliente, 15000.00, 13, dataInicio);
         
